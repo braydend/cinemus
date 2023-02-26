f292a1322d0716d65a748b4e894149e7c5797bcb
Brayden <braydendiluca@gmail.com>
2023-02-26 11:12:22 +1100

    Initialise frontend app

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>Movie List</title>
+	<meta name="description" content="Movie list app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Movie List
+	</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
