936c6ff1faa99f70ad3e8c3d81dc5ab3a080647c
Brayden <braydendiluca@gmail.com>
2023-02-26 11:12:22 +1100

    Initialise frontend app

---
+++ web/src/routes/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+</style>
