097e54dcaf7ecfc0f28d2f409a84c5d806d6e52c
Brayden <braydendiluca@gmail.com>
2023-03-19 11:04:38 +1100

    Add tests for show domain

--- server/test/mocks/handlers.ts
+++ server/test/mocks/handlers.ts
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 import { url } from "../../src/config";
-import { buildStubMovie } from "../api";
+import { buildStubMovie, buildStubShow } from "../api";
 import { mapApiResponseToMedia } from "../../src/domain/media";
 
 export const handlers = [
@@ -21,6 +21,26 @@ export const handlers = [
         total_pages: 1,
         total_results: 1,
         results: [movie],
+      })
+    );
+  }),
+
+  rest.get(`${url.TMDB_URL}/tv/:id`, (req, res, ctx) => {
+    const show = buildStubShow();
+
+    return res(ctx.status(200), ctx.json(show));
+  }),
+
+  rest.get(`${url.TMDB_URL}/search/tv`, (req, res, ctx) => {
+    const show = buildStubShow();
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        page: 1,
+        total_pages: 1,
+        total_results: 1,
+        results: [show],
       })
     );
   }),
