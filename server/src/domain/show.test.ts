679232dc34bf563f60f2ae4d606708c638342065
Brayden <braydendiluca@gmail.com>
2023-03-19 11:04:38 +1100

    Add tests for show domain

---
+++ server/src/domain/show.test.ts
@@ -0,0 +1,66 @@
+import { it, expect, describe, vi } from "vitest";
+import { Media } from "./media";
+import { getShow, searchShows } from "./show";
+import * as tmdb from "../../src/api/tmdb";
+
+describe("show domain", () => {
+  describe("getShow", () => {
+    it("hits API if not in cache", async () => {
+      const apiSpy = vi.spyOn(tmdb, "getShow");
+      const expectedShow: Media = {
+        __type: "show",
+        title: "Stub Show",
+        id: 12345,
+      };
+      const result = await getShow("12345");
+
+      expect(apiSpy).toHaveBeenCalledOnce();
+      expect(result).toStrictEqual(expectedShow);
+    });
+
+    it("retrieves from cache if it exists", async () => {
+      const apiSpy = vi.spyOn(tmdb, "getShow");
+      const expectedShow: Media = {
+        __type: "show",
+        title: "Stub Show",
+        id: 12345,
+      };
+      const result = await getShow("12345");
+
+      expect(apiSpy).not.toHaveBeenCalled();
+      expect(result).toStrictEqual(expectedShow);
+    });
+  });
+
+  describe("searchShows", () => {
+    it("hits API if not in cache", async () => {
+      const apiSpy = vi.spyOn(tmdb, "searchShows");
+      const expectedShows: Media[] = [
+        {
+          __type: "show",
+          title: "Stub Show",
+          id: 12345,
+        },
+      ];
+      const result = await searchShows("Stub");
+
+      expect(apiSpy).toHaveBeenCalledOnce();
+      expect(result).toStrictEqual(expectedShows);
+    });
+
+    it("retrieves from cache if it exists", async () => {
+      const apiSpy = vi.spyOn(tmdb, "getShow");
+      const expectedShows: Media[] = [
+        {
+          __type: "show",
+          title: "Stub Show",
+          id: 12345,
+        },
+      ];
+      const result = await searchShows("Stub");
+
+      expect(apiSpy).not.toHaveBeenCalled();
+      expect(result).toStrictEqual(expectedShows);
+    });
+  });
+});
