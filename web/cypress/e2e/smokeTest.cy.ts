798da5435750a4a05bc7a98dcad41214c871bb44
Brayden <braydendiluca@gmail.com>
2023-03-25 16:24:58 +1100

    fixup! Install cypress and add smoke test

--- web/cypress/e2e/smokeTest.cy.ts
+++ web/cypress/e2e/smokeTest.cy.ts
@@ -1,4 +1,4 @@
-describe("template spec", () => {
+describe("smoke test", () => {
   it("passes", () => {
     // cy.visit(Cypress.env("CYPRESS_REACT_URL"));
     cy.visit("http://127.0.0.1:5173/");
