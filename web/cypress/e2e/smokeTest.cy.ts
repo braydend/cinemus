676b794bfb3b8cba49e24290a136215ba9dd586f
Brayden <braydendiluca@gmail.com>
2023-03-29 19:50:42 +1100

    fixup! fixup! Install cypress and add smoke test

--- web/cypress/e2e/smokeTest.cy.ts
+++ web/cypress/e2e/smokeTest.cy.ts
@@ -1,7 +1,7 @@
 describe("smoke test", () => {
   it("passes", () => {
     // cy.visit(Cypress.env("CYPRESS_REACT_URL"));
-    cy.visit("http://127.0.0.1:5173/");
+    cy.visit("127.0.0.1:5173");
 
     cy.findByRole("heading", { name: "Cinemus" }).should("exist");
   });
