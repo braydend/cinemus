c8d02282587fb086e4e7a74f24eb935a2167ac1e
Brayden <braydendiluca@gmail.com>
2023-03-29 21:13:40 +1100

    Use MaterialUI to add app navigation

---
+++ web/src/components/molecules/UserMenu/UserMenu.tsx
@@ -0,0 +1,66 @@
+import { FC, MouseEvent, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const UserMenu: FC = () => {
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleLogout = () => {
+    logout();
+    handleCloseUserMenu();
+  };
+  return isAuthenticated ? (
+    <Box sx={{ flexGrow: 0 }}>
+      <>
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar src={user?.picture} alt={user?.name} />
+        </IconButton>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem onClick={handleLogout}>
+            <Typography textAlign="center">Logout</Typography>
+          </MenuItem>
+        </Menu>
+      </>
+    </Box>
+  ) : (
+    <Button
+      onClick={() => {
+        loginWithRedirect();
+      }}
+      variant="contained"
+      color="success"
+    >
+      Login
+    </Button>
+  );
+};
