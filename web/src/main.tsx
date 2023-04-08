fa7469ce32bc287a95bf048faa5d15ce333e4ffc
Brayden <braydendiluca@gmail.com>
2023-04-08 20:01:05 +1000

    Configure Sentry

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,10 +2,19 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App";
-import { auth } from "./utils/config";
+import { auth, sentry } from "./utils/config";
 
 const queryClient = new QueryClient();
+Sentry.init({
+  dsn: sentry.dsn,
+  environment: sentry.environment,
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  tracesSampleRate: sentry.tracesSampleRate,
+  replaysSessionSampleRate: sentry.sessionSampleRate,
+  replaysOnErrorSampleRate: sentry.errorSampleRate,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
