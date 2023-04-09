091979c20b372f1f92aa92e3e732c7d698c5e11e
Brayden <braydendiluca@gmail.com>
2023-04-09 16:04:28 +1000

    Add query devtools to frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { type FC } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Navigation } from "./components/molecules";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -15,6 +16,7 @@ const App: FC = () => {
         <Navigation />
         {isAuthenticated ? <MediaList /> : <div>Login to use Cinemus</div>}
       </Container>
+      <ReactQueryDevtools />
     </>
   );
 };
