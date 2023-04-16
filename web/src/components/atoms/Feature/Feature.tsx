6f6fe148a3b7190a10649d0badbc25fda83587b2
Brayden <braydendiluca@gmail.com>
2023-04-16 12:26:09 +1000

    Add FeatureList component

--- web/src/components/atoms/Feature/Feature.tsx
+++ web/src/components/atoms/Feature/Feature.tsx
@@ -1,16 +1,13 @@
 import { type FC } from "react";
-import styles from "./feature.module.css";
 import Typography from "@mui/material/Typography";
 
 interface Props {
   title: string;
   body: string;
-  icon: JSX.Element;
 }
 
-export const Feature: FC<Props> = ({ title, body, icon }) => (
-  <section key={title} className={styles.section}>
-    <div className={styles.iconContainer}>{icon}</div>
+export const Feature: FC<Props> = ({ title, body }) => (
+  <section key={title}>
     <Typography variant="h3" color={"primary"}>
       {title}
     </Typography>
--- web/src/components/molecules/index.ts
+++ web/src/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export * from "./Navigation";
 export * from "./UserMenu";
 export * from "./SearchBox";
+export * from "./FeatureList";
