bf7033cf7047dea8ac4b64b9f8c562122081b875
Brayden <braydendiluca@gmail.com>
2023-04-23 14:40:43 +1000

    Add mapping function to format media with watch providers

--- server/src/domain/media.ts
+++ server/src/domain/media.ts
@@ -2,8 +2,13 @@ import {
   type TmdbConfigurationResponse,
   type TmdbMovie,
   type TmdbShow,
+  type TmdbShowWatchProviderResponse,
 } from "../api";
 import { getImages, type Images } from "./image";
+import {
+  mapResponseToWatchProvider,
+  type WatchProvider,
+} from "./watchProviders";
 
 // TODO: remove this and replace with common typing
 export interface Media {
@@ -12,14 +17,30 @@ export interface Media {
   images: Images;
   __type: "movie" | "show";
   isWatched?: boolean;
+  watchProviders?: WatchProvider[];
 }
 
-export const mapApiResponseToMedia = async (
+export type MediaList = Array<Omit<Media, "watchProviders">>;
+
+export const mapApiResponseToMediaWithWatchProviders = (
+  response: TmdbMovie | TmdbShow,
+  configuration: TmdbConfigurationResponse,
+  watchProviders: TmdbShowWatchProviderResponse
+): Media => {
+  const media = mapApiResponseToMedia(response, configuration);
+
+  return {
+    ...media,
+    watchProviders: mapResponseToWatchProvider(watchProviders, configuration),
+  };
+};
+
+export const mapApiResponseToMedia = (
   response: TmdbMovie | TmdbShow,
   configuration: TmdbConfigurationResponse
-): Promise<Media> => {
+): Omit<Media, "watchProviders"> => {
   const isMovie = response.__type === "movie";
-  const images = await getImages(response.poster_path, configuration);
+  const images = getImages(response.poster_path, configuration);
 
   return {
     id: response.id,
@@ -27,4 +48,11 @@ export const mapApiResponseToMedia = async (
     images,
     __type: response.__type,
   };
+};
+
+export const mapApiResponseToMediaList = (
+  response: TmdbMovie[] | TmdbShow[],
+  configuration: TmdbConfigurationResponse
+): MediaList => {
+  return response.map((res) => mapApiResponseToMedia(res, configuration));
 };
