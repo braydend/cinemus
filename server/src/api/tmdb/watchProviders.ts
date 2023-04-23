77c30587b3faa45187fe74d1d3f5a8ae3f98e048
Brayden <braydendiluca@gmail.com>
2023-04-23 14:39:19 +1000

    Add TMDB API call get get watch providers for shows

--- server/src/api/tmdb/watchProviders.ts
+++ server/src/api/tmdb/watchProviders.ts
@@ -13,6 +13,29 @@ export interface TmdbWatchProviderRegion {
   native_name: string;
 }
 
+export interface TmdbShowWatchProviderResponse {
+  id: number;
+  results: Results;
+}
+
+export type Results = Record<string, WatchRegionDetails>;
+
+export interface WatchProviderDetails {
+  display_priority: number;
+  logo_path: string;
+  provider_id: number;
+  provider_name: string;
+}
+
+interface WatchRegionDetails {
+  link: string;
+  flatrate: WatchProviderDetails[];
+  buy?: WatchProviderDetails[];
+  ads?: WatchProviderDetails[];
+  rent?: WatchProviderDetails[];
+  free?: WatchProviderDetails[];
+}
+
 export const getWatchProviderRegions =
   async (): Promise<TmdbWatchProviderRegionsResponse> => {
     try {
@@ -23,7 +46,23 @@ export const getWatchProviderRegions =
       return response.data;
     } catch (e) {
       throw Error(
-        `Failed to get TMDB watch providers: ${(e as Error).message}`
+        `Failed to get TMDB watch provider regions: ${(e as Error).message}`
       );
     }
   };
+
+export const getShowWatchProviders = async (
+  id: string
+): Promise<TmdbShowWatchProviderResponse> => {
+  try {
+    const response = await axios.get<TmdbShowWatchProviderResponse>(
+      `${tmdbBaseUrl}/tv/${id}/watch/providers?api_key=${secrets.MOVIE_DB_API_KEY}`
+    );
+
+    return response.data;
+  } catch (e) {
+    throw Error(
+      `Failed to get TMDB  show watch providers: ${(e as Error).message}`
+    );
+  }
+};
