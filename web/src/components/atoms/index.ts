70fb1f52339aaeec11550108312881548daef189
Brayden <braydendiluca@gmail.com>
2023-05-05 10:45:21 +1000

    Remove Feature component

--- web/src/components/atoms/index.ts
+++ web/src/components/atoms/index.ts
@@ -2,6 +2,5 @@ export * from "./ListItem";
 export * from "./MediaTypeSelector";
 export * from "./Inspiration";
 export * from "./TmdbInfo";
-export * from "./Feature";
 export * from "./Select";
 export * from "./Card";
