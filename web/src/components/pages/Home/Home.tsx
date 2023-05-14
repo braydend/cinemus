842a6e005420a88a6d9a22d8a290f187cab7e15b
Brayden <braydendiluca@gmail.com>
2023-05-14 10:43:25 +1000

    Restyle homepage

--- web/src/components/pages/Home/Home.tsx
+++ web/src/components/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import { type FC } from "react";
 import styles from "./home.module.css";
-import { Divider } from "@mui/material";
 import { FeatureList } from "../../molecules";
 import textLogo from "../../../assets/textLogo.png";
 
@@ -9,8 +8,9 @@ export const Home: FC = () => {
     <main className={styles.main}>
       <img src={textLogo} alt="Cinemus" className={styles.textLogo} />
       <h2 className={styles.tagline}>Showtime Simplified</h2>
-      <Divider sx={{ margin: "1rem 0" }} />
-      <FeatureList />
+      <div className={styles.features}>
+        <FeatureList />
+      </div>
     </main>
   );
 };
