ed6b0c18c02eda539f28632faef2eccf747210b9
Brayden <braydendiluca@gmail.com>
2023-05-16 10:49:37 +1000

    Add Heading component

--- web/src/components/pages/About/About.tsx
+++ web/src/components/pages/About/About.tsx
@@ -1,16 +1,13 @@
 import { type FC } from "react";
-import Typography from "@mui/material/Typography";
 import { Divider } from "@mui/material";
-import { Inspiration, TmdbInfo } from "../../atoms";
+import { Heading, Inspiration, TmdbInfo } from "../../atoms";
 import { FeatureList } from "../../molecules";
 import styles from "./about.module.css";
 
 export const About: FC = () => {
   return (
     <main className={styles.container}>
-      <Typography noWrap variant="h2" paddingBottom="0.5rem">
-        Features
-      </Typography>
+      <Heading level="2">Features</Heading>
       <FeatureList />
       <Divider sx={{ margin: "1rem 0" }} />
       <Inspiration />
