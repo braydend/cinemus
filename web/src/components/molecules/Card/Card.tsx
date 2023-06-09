122e8568a978f7ade855b6878ef628400855a6b9
Brayden <braydendiluca@gmail.com>
2023-06-09 22:55:11 +1000

    Restyle card with tailwind

--- web/src/components/molecules/Card/Card.tsx
+++ web/src/components/molecules/Card/Card.tsx
@@ -1,5 +1,4 @@
 import { type FC } from "react";
-import styles from "./card.module.css";
 import TV from "../../../assets/television.png";
 import Search from "../../../assets/magnifying-glass.png";
 import Tick from "../../../assets/check-mark.png";
@@ -21,16 +20,22 @@ export const Card: FC<Props> = ({ title, body, icon, className = "" }) => {
   };
 
   return (
-    <section key={title} className={`${styles.container} ${className}`}>
+    <section
+      key={title}
+      className={`
+    bg-cinemus-gray rounded-md border-8 border-cinemus-purple text-cinemus-purple p-4
+      ${className}
+    `}
+    >
       <img
-        className={styles.icon}
+        className="p-2 rounded w-16 h-16 mb-4"
         src={icons[icon]}
         width={32}
         height={32}
         alt={`${icon} icon`}
       />
       <Heading level={"3"}>{title}</Heading>
-      <p className={styles.body}>{body}</p>
+      <p className="font-raleway py-4">{body}</p>
     </section>
   );
 };
