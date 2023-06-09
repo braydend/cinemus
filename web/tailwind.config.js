51046bd88606f809fb70a9745f9a0b6ffc991492
Brayden <braydendiluca@gmail.com>
2023-06-09 22:27:11 +1000

    Add custom colours and fonts to tailind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "cinemus-purple": "#79609f",
+      "cinemus-dark-purple": "#4b2366",
+      "cinemus-yellow": "#f8d089",
+      "cinemus-yellow-pink": "#e2bcae",
+      "cinemus-pale-purple": "#cab1d5",
+      "cinemus-gray": "#edf0f2",
+    },
+    extend: {
+      fontFamily: {
+        raleway: "Raleway",
+        algera: "Algera",
+      },
+    },
   },
   plugins: [],
 };
