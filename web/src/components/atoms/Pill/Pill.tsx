4d096007f8907954d0245b1c1d5556dbfddf80a5
Brayden <braydendiluca@gmail.com>
2023-06-09 21:48:55 +1000

    Restyle pill with tailwind

--- web/src/components/atoms/Pill/Pill.tsx
+++ web/src/components/atoms/Pill/Pill.tsx
@@ -1,10 +1,14 @@
 import { type FC } from "react";
-import styles from "./Pill.module.css";
+// import styles from "./Pill.module.css";
 
 interface Props {
   label: string;
 }
 
 export const Pill: FC<Props> = ({ label }) => {
-  return <span className={styles.pill}>{label}</span>;
+  return (
+    <span className="rounded border border-purple-950 whitespace-nowrap py-0 px-2 h-fit text-purple-950">
+      {label}
+    </span>
+  );
 };
