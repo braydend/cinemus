204d4da98cac5ae559e6b517f19b46a383c2a1ca
Brayden <braydendiluca@gmail.com>
2023-06-10 13:48:22 +1000

    Restyle TMDB info with tailwind

--- web/src/components/atoms/TmdbInfo/TmdbInfo.tsx
+++ web/src/components/atoms/TmdbInfo/TmdbInfo.tsx
@@ -1,18 +1,18 @@
 import { type FC } from "react";
 import { tmdb } from "../../../content";
-import styles from "./TmdbInfo.module.css";
 import { Heading } from "../Heading";
+
 export const TmdbInfo: FC = () => (
   <section>
     <Heading level="2">{tmdb.title}</Heading>
-    <p>{tmdb.body}</p>
-    <p>
+    <p className="py-4">{tmdb.body}</p>
+    <p className="py-4">
       {tmdb.disclaimer}{" "}
       <a
         target={"_blank"}
         href={tmdb.link}
         rel="noreferrer"
-        className={styles.link}
+        className="text-cinemus-purple visited:text-cinemus-purple underline"
       >
         {tmdb.link}
       </a>
