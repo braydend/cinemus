8748d07d04ddb2bbf2d7b451f0d30919fc637f15
Brayden <braydendiluca@gmail.com>
2023-06-12 11:11:07 +1000

    Add sentry

---
+++ cinemus/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://60f14eaec17a45d19db8fffc9e8bf8ef@o538041.ingest.sentry.io/4505343869714432",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 0.1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
