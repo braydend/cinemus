860ffa252b73c813c6ce9d0ca35dbcfd0fb02513
Brayden <braydendiluca@gmail.com>
2023-06-12 11:10:44 +1000

    Secure list and user pages

--- cinemus/src/pages/user/index.tsx
+++ cinemus/src/pages/user/index.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import { api } from "../../utils/api";
 import { Heading } from "../../components/atoms";
 import { Avatar } from "@mui/material";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { UserPreferences } from "../../components/molecules";
 
 const UserPage: NextPage = () => {
@@ -12,8 +12,6 @@ const UserPage: NextPage = () => {
   if (isLoading) {
     return <div>loading</div>;
   }
-
-  // if (Boolean(error)) throw Error(JSON.stringify(error));
 
   if (data == null) throw Error("Unable to fetch user preferences for user");
 
@@ -30,4 +28,4 @@ const UserPage: NextPage = () => {
   );
 };
 
-export default UserPage;
+export default withPageAuthRequired(UserPage);
