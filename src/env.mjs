89aec6fdb098803d5c073183d95224e1f9e2284c
Brayden <braydendiluca@gmail.com>
2023-06-16 19:29:29 +1200

    Use NextAuth with Google and GitHub

--- src/env.mjs
+++ src/env.mjs
@@ -9,20 +9,22 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
-    // NEXTAUTH_SECRET:
-    //   process.env.NODE_ENV === "production"
-    //     ? z.string().min(1)
-    //     : z.string().min(1).optional(),
-    // NEXTAUTH_URL: z.preprocess(
-    //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    //   (str) => process.env.VERCEL_URL ?? str,
-    //   // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    //   process.env.VERCEL ? z.string().min(1) : z.string().url()
-    // ),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string().min(1)
+        : z.string().min(1).optional(),
+    NEXTAUTH_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string().min(1) : z.string().url()
+    ),
     // // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-    // DISCORD_CLIENT_ID: z.string(),
-    // DISCORD_CLIENT_SECRET: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
     AUTH0_SECRET: z.string(),
     AUTH0_BASE_URL: z.string(),
     AUTH0_ISSUER_BASE_URL: z.string(),
@@ -59,10 +61,12 @@ export const env = createEnv({
     TMDB_URL: process.env.TMDB_URL,
     DATABASE_NAME: process.env.DATABASE_NAME,
     UPSTASH_REDIS_API_KEY: process.env.UPSTASH_REDIS_API_KEY,
-    // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
--- src/pages/api/auth/DISABLED[...nextauth].ts
+++
@@ -1,5 +0,0 @@
-// import NextAuth from "next-auth";
-// import { authOptions } from "~/server/auth";
-
-// Disabled to use Auth0
-// export default NextAuth(authOptions);
