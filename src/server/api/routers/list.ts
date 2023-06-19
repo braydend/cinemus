a1ec837e47be2761e767ed8ca23afec66d2cf9fd
Brayden <braydendiluca@gmail.com>
2023-06-19 17:01:14 -0400

    Clean up TRPC inputs

--- src/server/api/routers/list.ts
+++ src/server/api/routers/list.ts
@@ -8,25 +8,82 @@ import {
   updateList,
 } from "../../domain/list";
 import { getUserPreferences } from "../../domain/userPreferences";
-import { getUserDetails } from "../../domain/user";
+
+const getListByIdInput = z.string();
+const acceptInvitationInput = z.string();
+const updateListInput = z.object({
+  id: z.string(),
+  __type: z.enum(["movie", "show"]),
+  isWatched: z.boolean().optional(),
+  title: z.string(),
+  genres: z.string().array(),
+  watchProviders: z
+    .object({
+      region: z.string(),
+      flatrate: z
+        .object({
+          name: z.string(),
+          logoUrl: z.string(),
+        })
+        .array()
+        .optional(),
+      buy: z
+        .object({
+          name: z.string(),
+          logoUrl: z.string(),
+        })
+        .array()
+        .optional(),
+      ads: z
+        .object({
+          name: z.string(),
+          logoUrl: z.string(),
+        })
+        .array()
+        .optional(),
+      rent: z
+        .object({
+          name: z.string(),
+          logoUrl: z.string(),
+        })
+        .array()
+        .optional(),
+      free: z
+        .object({
+          name: z.string(),
+          logoUrl: z.string(),
+        })
+        .array()
+        .optional(),
+    })
+    .array()
+    .optional(),
+  images: z.object({
+    backdrop: z.record(z.string()),
+    logo: z.record(z.string()),
+    poster: z.record(z.string()),
+    profile: z.record(z.string()),
+    still: z.record(z.string()),
+  }),
+});
+
+const removeFromListInput = z.object({
+  __type: z.enum(["movie", "show"]),
+  id: z.string(),
+});
 
 export const listRouter = createTRPCRouter({
   getListById: protectedProcedure
-    .input(z.string())
+    .input(getListByIdInput)
     .query(async ({ ctx, input }) => {
-      const userId = (ctx.session.user["sub"] as string).split("|")[1] ?? "";
+      const userId = ctx.session.user.id;
       const prefs = await getUserPreferences(userId);
       return await getListById(input, prefs.watchProviderRegion ?? undefined);
     }),
-  test: protectedProcedure.query(async ({ ctx }) => {
-    const userId = (ctx.session.user["sub"] as string).split("|")[1] ?? "";
-    // const prefs = await getUserPreferences(userId);
-    await getUserDetails(userId, ctx.session.accessToken ?? "");
-  }),
   acceptInvitation: protectedProcedure
-    .input(z.string())
+    .input(acceptInvitationInput)
     .mutation(async ({ ctx, input }) => {
-      const userId = (ctx.session.user["sub"] as string).split("|")[1] ?? "";
+      const userId = ctx.session.user.id;
       // const prefs = await getUserPreferences(userId);
       return await joinList(input, userId);
     }),
@@ -36,65 +93,7 @@ export const listRouter = createTRPCRouter({
     return await getList(userId, prefs.watchProviderRegion ?? undefined);
   }),
   updateList: protectedProcedure
-    .input(
-      // z.array(
-      z.object({
-        id: z.string(),
-        __type: z.enum(["movie", "show"]),
-        isWatched: z.boolean().optional(),
-        title: z.string(),
-        genres: z.string().array(),
-        watchProviders: z
-          .object({
-            region: z.string(),
-            flatrate: z
-              .object({
-                name: z.string(),
-                logoUrl: z.string(),
-              })
-              .array()
-              .optional(),
-            buy: z
-              .object({
-                name: z.string(),
-                logoUrl: z.string(),
-              })
-              .array()
-              .optional(),
-            ads: z
-              .object({
-                name: z.string(),
-                logoUrl: z.string(),
-              })
-              .array()
-              .optional(),
-            rent: z
-              .object({
-                name: z.string(),
-                logoUrl: z.string(),
-              })
-              .array()
-              .optional(),
-            free: z
-              .object({
-                name: z.string(),
-                logoUrl: z.string(),
-              })
-              .array()
-              .optional(),
-          })
-          .array()
-          .optional(),
-        images: z.object({
-          backdrop: z.record(z.string()),
-          logo: z.record(z.string()),
-          poster: z.record(z.string()),
-          profile: z.record(z.string()),
-          still: z.record(z.string()),
-        }),
-      })
-      // )
-    )
+    .input(updateListInput)
     .mutation(async ({ ctx, input }) => {
       const userId = ctx.session.user.id;
       const prefs = await getUserPreferences(userId);
@@ -106,12 +105,7 @@ export const listRouter = createTRPCRouter({
       );
     }),
   removeFromList: protectedProcedure
-    .input(
-      z.object({
-        __type: z.enum(["movie", "show"]),
-        id: z.string(),
-      })
-    )
+    .input(removeFromListInput)
     .mutation(async ({ input, ctx }) => {
       const userId = ctx.session.user.id;
       const prefs = await getUserPreferences(userId);
