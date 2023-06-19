8eac2a7535530e855529b2467a3d5469634c63a0
Brayden <braydendiluca@gmail.com>
2023-06-19 17:00:54 -0400

    Build invitation page

--- src/pages/list/[id]/invitation/index.tsx
+++ src/pages/list/[id]/invitation/index.tsx
@@ -1,10 +1,14 @@
-import { Heading } from "~/components/atoms";
+import { Button, Heading, ImageStack } from "~/components/atoms";
 import { type NextPage } from "next";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { useRouter } from "next/router";
 import { api } from "../../../../utils/api";
+import { useAuthRequired } from "../../../../hooks/useAuthRequired";
+import { availableRoutes } from "../../../../routes";
+import Link from "next/link";
 
 const ListInvitationPage: NextPage = () => {
+  useAuthRequired();
+  const router = useRouter();
   const {
     query: { id },
   } = useRouter();
@@ -13,33 +17,71 @@ const ListInvitationPage: NextPage = () => {
     (typeof id === "string" ? id : id !== undefined ? id[0] : "") ?? "";
 
   const { data, isLoading } = api.listRouter.getListById.useQuery(listId);
-  api.listRouter.test.useQuery();
-  const { mutate } = api.listRouter.acceptInvitation.useMutation();
+  const { mutate: joinList, isLoading: isJoinLoading } =
+    api.listRouter.acceptInvitation.useMutation({
+      onSuccess: () => {
+        void router.push(`${availableRoutes.list}/${listId}`);
+      },
+    });
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
 
   if (!data) {
     return <div> no list</div>;
   }
 
+  const owner = data.owner;
+
+  const listMembers = data.members.map(({ user: { name, email, image } }) => ({
+    name,
+    email,
+    image,
+  }));
+
   return (
-    <main className="font-raleway text-cinemus-purple">
-      {isLoading ? (
-        <div>loading</div>
-      ) : (
-        <>
-          <Heading level="2">
-            {`You've been invited to join a list (#${data?.id})`}
-          </Heading>
-          <button
+    <main className="flex flex-col items-center font-raleway text-cinemus-purple">
+      <Heading level="2">
+        {`You've been invited to join ${
+          data.owner.name ?? data.owner.email ?? "someone"
+        }'s list`}
+      </Heading>
+      <ImageStack
+        className="mt-8"
+        images={[
+          {
+            src: owner.image ?? "",
+            alt: `${owner.name ?? owner.email ?? "list owner"}'s icon`,
+          },
+          ...listMembers.map(({ image, name, email }, index) => ({
+            alt: `${name ?? email ?? `member#${index}`}'s icon`,
+            src: image ?? "",
+          })),
+        ]}
+      />
+      <div className="mt-8 grid w-1/2 grid-cols-2 gap-4">
+        <Button
+          onClick={() => {
+            joinList(listId);
+          }}
+          label={isJoinLoading ? "Joining" : "Accept"}
+          variant="purple"
+          disabled={isJoinLoading}
+        />
+        <Link href={availableRoutes.list}>
+          <Button
             onClick={() => {
-              mutate(listId);
+              return;
             }}
-          >
-            Accept
-          </button>
-        </>
-      )}
+            label="No thanks"
+            variant="purple"
+            className="w-full"
+          />
+        </Link>
+      </div>
     </main>
   );
 };
 
-export default withPageAuthRequired(ListInvitationPage);
+export default ListInvitationPage;
