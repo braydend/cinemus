c8862d0e9c3e936f3a9f82099c6de91a90606c0f
Brayden <braydendiluca@gmail.com>
2023-07-01 17:19:33 +1000

    Move custom colours to tailwind extention to allow access to base colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,18 +3,18 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "cinemus-purple": "#79609f",
-      "cinemus-dark-purple": "#4b2366",
-      "cinemus-yellow": "#f8d089",
-      "cinemus-yellow-pink": "#e2bcae",
-      "cinemus-pale-purple": "#cab1d5",
-      "cinemus-gray": "#edf0f2",
-    },
     extend: {
       fontFamily: {
         raleway: "Raleway",
         algera: "Algera",
+      },
+      colors: {
+        "cinemus-purple": "#79609f",
+        "cinemus-dark-purple": "#4b2366",
+        "cinemus-yellow": "#f8d089",
+        "cinemus-yellow-pink": "#e2bcae",
+        "cinemus-pale-purple": "#cab1d5",
+        "cinemus-gray": "#edf0f2",
       },
     },
   },
