a2f32a230e80c5a9d53f9f16221e41571db9804d
Brayden <braydendiluca@gmail.com>
2023-07-02 09:55:55 +1000

    Create own Avatar component

--- src/components/molecules/UserStack/UserStack.tsx
+++ src/components/molecules/UserStack/UserStack.tsx
@@ -1,16 +1,31 @@
 import { type FC } from "react";
-import { ImageStack } from "../../atoms";
+import { Avatar } from "../../atoms";
 import { type User } from "@prisma/client";
 
 type Props = {
-  users: User[];
+  users: Omit<User, "emailVerified">[];
+};
+
+const MAX_IMAGES = 2;
+
+const Counter: FC<{ count: number }> = ({ count }) => {
+  return (
+    <div className="mr-[-12px] flex h-16 w-16 items-center justify-center rounded-full bg-cinemus-purple text-3xl text-cinemus-yellow-pink">
+      +{count}
+    </div>
+  );
 };
 
 export const UserStack: FC<Props> = ({ users }) => {
-  const imageData = users.map(({ image, name, email }) => ({
-    src: image ?? "",
-    alt: `${name ?? email ?? "someone"}'s icon`,
-  }));
+  const limitedImages = users.slice(0, MAX_IMAGES);
+  const hiddenImageCount = users.length - MAX_IMAGES;
 
-  return <ImageStack images={imageData} />;
+  return (
+    <div className={`flex flex-row`}>
+      {limitedImages.map((user) => (
+        <Avatar user={user} key={JSON.stringify(user)} className="mr-[-12px]" />
+      ))}
+      {hiddenImageCount > 0 && <Counter count={hiddenImageCount} />}
+    </div>
+  );
 };
