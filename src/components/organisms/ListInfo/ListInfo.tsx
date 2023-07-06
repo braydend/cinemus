b4f96ad0274ae4d2069789a6fbba8d077108a596
Brayden <braydendiluca@gmail.com>
2023-07-06 16:35:41 +1000

    Update List UI to support editing members

--- src/components/organisms/ListInfo/ListInfo.tsx
+++ src/components/organisms/ListInfo/ListInfo.tsx
@@ -2,24 +2,23 @@ import { type inferRouterOutputs } from "@trpc/server";
 import { enqueueSnackbar } from "notistack";
 import { type FC, useState, useEffect, useRef } from "react";
 import { type AppRouter } from "../../../server/api/root";
-import { Button, Heading } from "../../atoms";
+import { Avatar, Button, Heading } from "../../atoms";
 import { UserStack } from "../../molecules";
 import { useRouter } from "next/router";
 import { api } from "../../../utils/api";
 import { useSession } from "next-auth/react";
 import { availableRoutes } from "../../../routes";
+import { ArrayElement } from "../../../utils/types";
 
 type ListData = inferRouterOutputs<AppRouter>["listRouter"]["getListData"];
+type Role = "VIEWER" | "COLLABORATOR" | "MODERATOR";
 
 type Props = { data: ListData };
 
 export const ListInfo: FC<Props> = ({ data }) => {
-  const deletePrompt = useRef<HTMLDialogElement>(null);
-  const { asPath, push: routerPush } = useRouter();
+  const { asPath } = useRouter();
   const session = useSession();
-  const trpcContext = api.useContext();
 
-  const [listName, setListName] = useState(data.name);
   const [isEditMode, setIsEditMode] = useState(false);
   const [origin, setOrigin] = useState("");
 
@@ -28,6 +27,59 @@ export const ListInfo: FC<Props> = ({ data }) => {
       setOrigin(window.location.origin);
     }
   }, []);
+
+  const canEdit = data.ownerId === session.data?.user.id;
+
+  return isEditMode ? (
+    <EditListInfo
+      initialData={data}
+      exitEditMode={() => setIsEditMode(false)}
+    />
+  ) : (
+    <div className="flex flex-col justify-between pb-4">
+      <div className="flex flex-row flex-wrap items-center gap-4">
+        <div className="flex flex-grow flex-row items-center justify-between">
+          <Heading level="2" className="break-words">
+            {data.name}
+          </Heading>
+          {canEdit && (
+            <Button
+              label="Edit"
+              onClick={() => setIsEditMode(true)}
+              variant="purple"
+              className="h-fit"
+            />
+          )}
+        </div>
+      </div>
+      <div className="flex h-fit flex-row items-center gap-4">
+        <UserStack users={[data.owner, ...data.members]} />
+        <Button
+          label="Invite"
+          variant="purple"
+          className="h-fit"
+          onClick={() => {
+            const invitationLink = `${origin}${asPath}/invitation`;
+            void navigator.clipboard.writeText(invitationLink);
+            enqueueSnackbar("Invitation link copied to clipboard!", {
+              variant: "info",
+            });
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+const EditListInfo: FC<{ initialData: ListData; exitEditMode: () => void }> = ({
+  initialData,
+  exitEditMode,
+}) => {
+  const deletePrompt = useRef<HTMLDialogElement>(null);
+  const trpcContext = api.useContext();
+  const { push: routerPush } = useRouter();
+  const [listName, setListName] = useState(initialData.name);
+  const [members, setMembers] = useState(initialData.members);
 
   const { mutate: editList, isLoading: isEditLoading } =
     api.listRouter.updateListData.useMutation({
@@ -42,8 +94,8 @@ export const ListInfo: FC<Props> = ({ data }) => {
           message: "Successfully updated list!",
           variant: "success",
         });
-        setIsEditMode(false);
-        void trpcContext.listRouter.getListData.invalidate(data.id);
+        exitEditMode();
+        void trpcContext.listRouter.getListData.invalidate(initialData.id);
         setListName(updatedList.name);
       },
     });
@@ -67,7 +119,11 @@ export const ListInfo: FC<Props> = ({ data }) => {
     });
 
   const handleSaveChanges = () => {
-    editList({ listId: data.id, name: listName });
+    editList({
+      listId: initialData.id,
+      name: listName,
+      members: members.map(({ id, role }) => ({ id, role })),
+    });
   };
 
   const handleLaunchDeleteModal = () => {
@@ -79,37 +135,34 @@ export const ListInfo: FC<Props> = ({ data }) => {
   };
 
   const handleDelete = () => {
-    deleteList(data.id);
+    deleteList(initialData.id);
   };
 
-  const canEdit = data.ownerId === session.data?.user.id;
+  const handleCancelEdit = () => {
+    setListName(initialData.name);
+    setMembers(initialData.members);
+    exitEditMode();
+  };
+
+  const handleUpdateListMembers = (
+    member: ArrayElement<ListData["members"]>
+  ) => {
+    setMembers((prev) => [
+      ...prev.filter(({ id: userId }) => userId !== member.id),
+      member,
+    ]);
+  };
 
   return (
     <div className="flex flex-col justify-between pb-4">
       <div className="flex flex-row flex-wrap items-center gap-4">
-        {isEditMode ? (
-          <input
-            type="text"
-            value={listName}
-            className="h-min flex-grow rounded-sm border border-cinemus-purple px-2 py-1"
-            onChange={({ target: { value } }) => setListName(value)}
-          />
-        ) : (
-          <div className="flex flex-grow flex-row items-center justify-between">
-            <Heading level="2" className="break-words">
-              {listName}
-            </Heading>
-            {canEdit && (
-              <Button
-                label="Edit"
-                onClick={() => setIsEditMode(true)}
-                variant="purple"
-                className="h-fit"
-              />
-            )}
-          </div>
-        )}
-        {isEditMode && (
+        <input
+          type="text"
+          value={listName}
+          className="h-min flex-grow rounded-sm border border-cinemus-purple px-2 py-1"
+          onChange={({ target: { value } }) => setListName(value)}
+        />
+        <div className="flex flex-col">
           <div className={`flex flex-row items-center gap-2 pb-2 md:pb-0`}>
             <Button
               label={isEditLoading ? "Saving" : "Save"}
@@ -125,33 +178,41 @@ export const ListInfo: FC<Props> = ({ data }) => {
             />
             <Button
               label="Cancel"
-              onClick={() => setIsEditMode(false)}
+              onClick={handleCancelEdit}
               variant="purple"
             />
           </div>
-        )}
+        </div>
       </div>
-      <div className="flex h-fit flex-row items-center gap-4">
-        <UserStack users={[data.owner, ...data.members]} />
-        <Button
-          label="Invite"
-          variant="purple"
-          className="h-fit"
-          onClick={() => {
-            const invitationLink = `${origin}${asPath}/invitation`;
-            void navigator.clipboard.writeText(invitationLink);
-            enqueueSnackbar("Invitation link copied to clipboard!", {
-              variant: "info",
-            });
-          }}
-        />
+      <span>Members</span>
+      <div className={`flex flex-row items-center gap-2 pb-2 md:pb-0`}>
+        <div className="flex flex-col items-center gap-2">
+          <Avatar user={initialData.owner} />
+          <span>Owner</span>
+        </div>
+        {members.map((member) => (
+          <div key={member.id} className="flex flex-col items-center gap-2">
+            <Avatar user={member} showTooltip />
+            <select
+              value={member.role}
+              onChange={({ target: { value } }) =>
+                handleUpdateListMembers({ ...member, role: value as Role })
+              }
+              className="rounded-sm border border-cinemus-purple px-2 py-1"
+            >
+              <option value="VIEWER">Viewer</option>
+              <option value="COLLABORATOR">Collaborator</option>
+              <option value="MODERATOR">Moderator</option>
+            </select>
+          </div>
+        ))}
       </div>
       <dialog
         className="absolute left-0 right-0 mx-auto w-96 rounded-md border-4 border-cinemus-purple backdrop:bg-slate-500 backdrop:bg-opacity-60 open:z-10 open:flex open:flex-col"
         ref={deletePrompt}
       >
         <div>
-          Are you sure you want to delete <span>{data.name}</span>?
+          Are you sure you want to delete <span>{initialData.name}</span>?
         </div>
         <div>Once a list is deleted, there&apos;s no way to recover it.</div>
         <div className="flex flex-row justify-between pt-4">
