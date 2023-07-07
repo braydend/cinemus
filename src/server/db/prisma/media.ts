bf2e2eac53c6983b2abed476af0354070741f035
Brayden <braydendiluca@gmail.com>
2023-07-07 14:51:49 +1000

    Add custom error for adding duplicate media to a list

--- src/server/db/prisma/media.ts
+++ src/server/db/prisma/media.ts
@@ -1,13 +1,27 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "../../db";
+import { UserError } from "../../../errors";
 
 const addMediaToList = async (
   listId: string,
   mediaId: string,
   mediaType: string
 ) => {
-  return await prisma.media.create({
-    data: { id: mediaId, type: mediaType, listId },
-  });
+  try {
+    return await prisma.media.create({
+      data: { id: mediaId, type: mediaType, listId },
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        throw new UserError({
+          code: "BAD_REQUEST",
+          message: "This already exists in the list.",
+        });
+      }
+    }
+    throw e;
+  }
 };
 
 const prismaMediaFunctions = {
