7be8399568cc5ba6db77935a45c290def2c0e001
braydend <braydendiluca@gmail.com>
2023-07-12 15:50:33 +1000

    Configure PWA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,22 +6,15 @@ import textLogo from "~/assets/textLogo.png";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Cinemus</title>
-        <meta name="description" content="Showtime Simplified" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <main className="flex flex-col items-center">
-        <Image src={textLogo} alt="Cinemus" className="w-full md:w-3/4" />
-        <h2 className="m-0 inline shrink pb-8 text-center font-raleway text-6xl text-cinemus-purple">
-          Showtime Simplified
-        </h2>
-        <div className="flex h-full flex-col justify-around">
-          <FeatureList />
-        </div>
-      </main>
-    </>
+    <main className="flex flex-col items-center">
+      <Image src={textLogo} alt="Cinemus" className="w-full md:w-3/4" />
+      <h2 className="m-0 inline shrink pb-8 text-center font-raleway text-6xl text-cinemus-purple">
+        Showtime Simplified
+      </h2>
+      <div className="flex h-full flex-col justify-around">
+        <FeatureList />
+      </div>
+    </main>
   );
 };
 
