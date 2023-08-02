1078f3b5afe8e571d297a8d733d0129a42e8c4f9
Brayden <braydendiluca@gmail.com>
2023-08-02 22:28:09 +0800

    Add basic user emulation for admins

--- src/server/db/prisma/user.ts
+++ src/server/db/prisma/user.ts
@@ -4,10 +4,22 @@ const getUserById = async (userId: string) => {
   return await prisma.user.findUniqueOrThrow({ where: { id: userId } });
 };
 
+const getLatestSessionForUser = async (userId: string) => {
+  return await prisma.session.findFirst({
+    where: { userId },
+    orderBy: { expires: "desc" },
+    select: { sessionToken: true },
+  });
+};
+
 const getAllUsers = async () => {
   return await prisma.user.findMany({ include: { sessions: true } });
 };
 
-const prismaUserFunctions = { getUserById, getAllUsers };
+const prismaUserFunctions = {
+  getUserById,
+  getAllUsers,
+  getLatestSessionForUser,
+};
 
 export default prismaUserFunctions;
