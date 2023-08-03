921aa5b218b6a4fc385da128322f4132dc7b9792
Brayden <braydendiluca@gmail.com>
2023-08-03 08:10:11 +0800

    Revert "Add basic user emulation for admins"
    This reverts commit 2bc36ce8c9c2ae95831575608273fdc91d05df36.

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,9 @@
-import { type GetServerSidePropsContext, type NextPage } from "next";
+import { GetServerSidePropsContext, type NextPage } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../server/auth";
 import { api } from "../../utils/api";
 import { Heading } from "../../components/atoms";
 import dayjs from "dayjs";
-import Link from "next/link";
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
@@ -72,9 +71,6 @@ const AdminPage: NextPage = () => {
                       >
                         {latestSession?.expires.toDateString()}
                       </span>
-                      <Link href={`/api/admin/emulateUser?userId=${user.id}`}>
-                        Emulate
-                      </Link>
                     </>
                   )}
                 </li>
