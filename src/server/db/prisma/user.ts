6c0bff65bb86d515c7bc459dfa83e64d45f0e768
Brayden <braydendiluca@gmail.com>
2023-08-03 08:10:11 +0800

    Revert "Add basic user emulation for admins"
    This reverts commit 2bc36ce8c9c2ae95831575608273fdc91d05df36.

--- src/server/db/prisma/user.ts
+++ src/server/db/prisma/user.ts
@@ -4,22 +4,10 @@ const getUserById = async (userId: string) => {
   return await prisma.user.findUniqueOrThrow({ where: { id: userId } });
 };
 
-const getLatestSessionForUser = async (userId: string) => {
-  return await prisma.session.findFirst({
-    where: { userId },
-    orderBy: { expires: "desc" },
-    select: { sessionToken: true },
-  });
-};
-
 const getAllUsers = async () => {
   return await prisma.user.findMany({ include: { sessions: true } });
 };
 
-const prismaUserFunctions = {
-  getUserById,
-  getAllUsers,
-  getLatestSessionForUser,
-};
+const prismaUserFunctions = { getUserById, getAllUsers };
 
 export default prismaUserFunctions;
--- src/server/domain/user/user.ts
+++ src/server/domain/user/user.ts
@@ -3,7 +3,3 @@ import user from "~/server/db/prisma/user";
 export const getAllUsers = async () => {
   return await user.getAllUsers();
 };
-
-export const getLatestSessionForUser = async (userId: string) => {
-  return await user.getLatestSessionForUser(userId);
-};
